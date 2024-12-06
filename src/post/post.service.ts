db6af1f0418f462c9395cad28927e65482dacae3
Mahmoud-Mehri <mahmoud.mehri90@gmail.com>
2024-12-06 18:15:00 +0000

    Add new function for downloading image

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,10 @@
-import { HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { HttpStatus, Inject, Injectable, Logger } from '@nestjs/common';
+import * as fsp from 'fs/promises';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as https from 'https';
+import * as http from 'http';
+import * as crypto from 'crypto';
 import { IPostRepository } from './model/repository.intf';
 import { CreatePostDto } from './dto/post.create.dto';
 import { PagingDto, ServiceResponse } from 'src/common/types';
@@ -7,9 +13,56 @@ import { Post } from './model/models';
 
 @Injectable()
 export class PostService {
+  private logger: Logger;
   constructor(
     @Inject('IPostRepository') private postRepository: IPostRepository,
-  ) {}
+  ) {
+    this.logger = new Logger('Post Service');
+  }
+
+  async downloadImage(imageUrl: string): Promise<string | null> {
+    try {
+      const imageDir = path.join(process.cwd(), 'images');
+      await fsp.mkdir(imageDir, { recursive: true });
+
+      const fileExt = path.extname(new URL(imageUrl).pathname);
+      const uuid = crypto.randomUUID();
+      const filename = `${uuid}${fileExt}`;
+      const filePath = path.join(imageDir, filename);
+
+      await new Promise((resolve, reject) => {
+        const client = imageUrl.startsWith('https') ? https : http;
+        const request = client.get(imageUrl, (response) => {
+          if (response.statusCode !== 200) {
+            reject(new Error(`Image Download Failed: ${response.statusCode}`));
+
+            return;
+          }
+
+          const fileStream = fs.createWriteStream(filePath);
+          response.pipe(fileStream);
+
+          fileStream.on('finish', () => {
+            fileStream.close();
+            resolve(true);
+          });
+
+          fileStream.on('error', (err) => {
+            reject(err);
+          });
+        });
+
+        request.on('error', (err) => {
+          reject(err);
+        });
+      });
+
+      return filePath;
+    } catch (err) {
+      this.logger.error('Image Download Failed:', err);
+      return null;
+    }
+  }
 
   async createPost(
     userId: string,
@@ -18,11 +71,16 @@ export class PostService {
     try {
       // User permission can be checked here if not handled by Roles
 
+      let localImageAddr: string | null = null;
+      if (postInfo.imageUrl && postInfo.imageUrl.trim() !== '') {
+        localImageAddr = await this.downloadImage(postInfo.imageUrl);
+      }
+
       const post = new Post(
         null,
         postInfo.title,
         postInfo.content,
-        postInfo.imageUrl,
+        localImageAddr,
         userId,
       );
       const newPost = await this.postRepository.createPost(post);
