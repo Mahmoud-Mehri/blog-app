424b9b27fea1083d00244befd840e220fb5c2852
Mahmoud-Mehri <mahmoud.mehri90@gmail.com>
2024-12-07 01:06:43 +0000

    Change Update Post DTO to make properties optional

--- src/post/dto/post.update.dto.ts
+++ src/post/dto/post.update.dto.ts
@@ -1,14 +1,17 @@
-import { IsString, Length } from 'class-validator';
+import { IsOptional, IsString, Length } from 'class-validator';
 
 export class UpdatePostDto {
+  @IsOptional()
   @IsString()
   @Length(3, 100)
-  title: string;
+  title?: string;
 
+  @IsOptional()
   @Length(3, 1000)
-  content: string;
+  content?: string;
 
+  @IsOptional()
   @IsString()
   @Length(0, 255)
-  imageUrl: string;
+  imageUrl?: string;
 }
